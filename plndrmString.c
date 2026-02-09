433bd098dbba39c28202f3a42c09c132b20fa89c
Parth21064 <bidhanmandal.2106@gmail.com>
2026-02-09 22:10:53 +0530

    palindrome

--- plndrmString.c
+++ plndrmString.c
@@ -2,7 +2,24 @@
 
 int main(){
     char str[50];
-    printf("Enter ur String: ")
-    
+    printf("Enter ur String: ");
+    scanf("%s",str);
+
+    int len,i;
+    for(len=0;str[len]!='\0';len++);
+
+    for(i=0;i<len/2;i++){
+       if(str[i]!=str[len-i-1]){
+        break;
+       }
+    }
+
+    if(i==(len/2))
+    printf("palindrome...");
+
+    else
+    printf("Not a palindrome..");
+
+
     return 0;
 }
