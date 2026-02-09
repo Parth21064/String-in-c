5a39a489dc055c8d95caa6d18d3aa91ec0543ad4
Parth21064 <bidhanmandal.2106@gmail.com>
2026-02-09 21:50:55 +0530

    re

---
+++ plndrmString.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    char str[50];
+    printf("Enter ur String: ")
+    
+    return 0;
+}
--- rev.c
+++ rev.c
@@ -23,7 +23,7 @@ int main(){
         name[i]=name[i]^name[len-i-1];
     }
 
-    printf("%s",name);
+    printf("\n%s",name);
 
     return 0;
 }
